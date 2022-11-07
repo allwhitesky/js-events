3314ae2113bf2de222ccc9182c3129dd16f71cad
allwhitesky <allwhitesky@gmail.com>
2022-11-06 16:35:11 -0800

    adding post functionality

---
+++ index.js
@@ -0,0 +1,26 @@
+window.addEventListener("click", function () {
+    console.log("== The window was clicked")
+})
+
+function boxClickHandler(event){
+    console.log("== A box was clicked")
+    console.log("-- event.target:", event.target)
+    console.log("-- event.currentTarget:", event.currentTarget)
+}
+
+var boxes = document.getElementsByClassName("box")
+for(var i = 0; i < boxes.length; i++){
+    boxes[i].addEventListener("click", boxClickHandler)
+}
+
+function buttonClickHandler(event){
+    console.log("== A button was clicked")
+    console.log("-- event.target:", event.target)
+    console.log("-- event.currentTarget:", event.currentTarget)
+    event.stopPropagation()
+}
+
+var buttons = document.getElementsByClassName("in-box-button")
+for(var i = 0; i < buttons.length; i++){
+    buttons[i].addEventListener("click", buttonClickHandler)
+}
